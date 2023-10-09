2c09c1d769c6b9b187b7c5fab24f2bb208fc923f
Dtheran <dtheran1.0@gmail.com>
2023-10-08 21:53:52 -0500

    validaciones con los typos y validationsPipes

--- src/dtos/products.dto.ts
+++ src/dtos/products.dto.ts
@@ -1,15 +1,31 @@
+import { PartialType } from '@nestjs/mapped-types';
+import {
+  IsString,
+  IsNumber,
+  IsUrl,
+  IsNotEmpty,
+  IsPositive,
+} from 'class-validator';
+
 export class CreateProductDto {
+  @IsString()
+  @IsNotEmpty()
   readonly name: string;
+  @IsString()
+  @IsNotEmpty()
   readonly description: string;
+  @IsNumber()
+  @IsNotEmpty()
+  @IsPositive()
   readonly price: number;
+  @IsUrl()
+  @IsNotEmpty()
   readonly image: string;
+  @IsNumber()
+  @IsNotEmpty()
+  @IsPositive()
   readonly stock: number;
 }
 
-export class UpdateProductDto {
-  readonly name?: string;
-  readonly description?: string;
-  readonly price?: number;
-  readonly image?: string;
-  readonly stock?: number;
-}
+// Opcional
+export class UpdateProductDto extends PartialType(CreateProductDto) {}
